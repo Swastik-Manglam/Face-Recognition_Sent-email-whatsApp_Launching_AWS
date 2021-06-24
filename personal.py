af6ab29f4eb1d24dae58871996ed71793272b085
Swastik-Manglam <75330553+Swastik-Manglam@users.noreply.github.com>
2021-06-24 11:25:53 +0530

    Add files via upload

---
+++ personal.py
@@ -0,0 +1,8 @@
+def whatsapp_no():
+    return "+918228949020"
+def password():
+    return 'Swastik#123'
+def receiver_email():
+    return '1809054@kiit.ac.in'
+def sender_email():
+    return 'mirror010image@gmail.com'
